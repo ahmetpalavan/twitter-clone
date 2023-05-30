8206a34619af40ba12d88412151631b33cc6bcc6
ahmetpalavan <ahmetpalavann@gmail.com>
2023-05-30 22:43:14 +0300

    session and toaster added to main file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,18 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 import LoginModal from "@/modals/LoginModalnext-13";
 import { RegisterModal } from "@/modals/RegisterModalnext-13";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
       <LoginModal />
       <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
