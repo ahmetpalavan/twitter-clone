5a6b755a626e2647f4fcf9c6dfba302ce20f5764
ahmetpalavan <ahmetpalavann@gmail.com>
2023-05-31 21:49:41 +0300

    avatar and username added to followBar

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import useUsers from "@/hooks/useUsersnext-13";
+import Avatar from "../Avatar";
 
 const FollowBar = () => {
+  const { data: users = [] } = useUsers();
+
+  if (users.length === 0) {
+    return null;
+  }
+
   return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
-        <div className="mt-4 gap-6 flex flex-col">{/* To User List */}</div>
+        <div className="flex flex-col gap-6 mt-4">
+          {users.map((user: Record<string, any>) => (
+            <div key={user.id} className="flex flex-row gap-4">
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-white font-semibold text-sm">{user.name}</p>
+                <p className="text-neutral-400 text-sm">@{user.name}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,15 +1,36 @@
+import { useCurrentUser } from "@/hooks/useCurrentUsernext-13";
+import useLoginModal from "@/hooks/useLoginModalnext-13";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
 import { IconType } from "react-icons";
+
 
 interface SidebarItemProps {
   label: string;
   href?: string;
   icon: IconType;
   onClick?: () => void;
+  auth?: boolean;
 }
 
-const SidebarItem = ({ label, href, icon: Icon, onClick }: SidebarItemProps) => {
+const SidebarItem = ({ label, href, icon: Icon, onClick, auth }: SidebarItemProps) => {
+  const loginModal = useLoginModal();
+  const { push } = useRouter();
+  const { data: currentUser } = useCurrentUser();
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    if (auth && !currentUser) {
+      loginModal.open();
+      toast.error("You must login first");
+      return;
+    } else if (href) {
+      push(href);
+    }
+  };
   return (
-    <div className="flex flex-row items-center">
+    <div className="flex flex-row items-center" onClick={handleClick}>
       <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
         <Icon size={28} color="white" onClick={onClick} />
       </div>
