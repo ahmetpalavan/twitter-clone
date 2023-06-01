65112ee1aef8aab26e689ed4da7ce8ef9a4f4323
ahmetpalavan <ahmetpalavann@gmail.com>
2023-06-02 01:44:41 +0300

    post to homepage added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import Form from "@/components/Formnext-13";
 import Header from "@/components/Headernext-13";
+import PostFeed from "@/components/posts/PostFeednext-13";
+import { useCurrentUser } from "@/hooks/useCurrentUsernext-13";
 
 export default function Home() {
+  const { data: postId } = useCurrentUser();
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What's happening?" />
+      <PostFeed />
     </>
-  )
+  );
 }
